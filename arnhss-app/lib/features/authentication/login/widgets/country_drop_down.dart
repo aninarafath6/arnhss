8a9545a86904d8328a920e46b8fe56f999b4fb6a
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-11 18:35:49 +0530

    dropdown finished:

--- arnhss-app/lib/features/authentication/login/widgets/country_drop_down.dart
+++ arnhss-app/lib/features/authentication/login/widgets/country_drop_down.dart
@@ -10,65 +10,71 @@ class CountryDropDown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Material(
-        color: Colors.transparent,
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
         child: Container(
-          height: context.getHeight(90),
-          width: context.getWidth(80),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(
-              8,
-            ),
-          ),
+          margin: const EdgeInsets.all(30),
           child: FutureBuilder<List<CountryModel>>(
             future: context.watch<CountryViewModel>().result,
             builder: (context, snapshot) {
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 30.0, bottom: 15, left: 20, right: 20),
-                    child: TextField(
-                      onChanged: context.read<CountryViewModel>().searchCountry,
-                      cursorColor: CustomColors.dark,
-                      style: Theme.of(context).textTheme.subtitle1,
-                      decoration: const InputDecoration(
-                        hintText: "Search you country code...",
+              if (!snapshot.hasData) {
+                return const SizedBox();
+              }
+              return Container(
+                height: context.getHeight(snapshot.data!.isEmpty ? 20 : 90),
+                width: context.getWidth(80),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    8,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 30.0, bottom: 15, left: 20, right: 20),
+                      child: TextField(
+                        onChanged:
+                            context.read<CountryViewModel>().searchCountry,
+                        cursorColor: CustomColors.dark,
+                        style: Theme.of(context).textTheme.subtitle1,
+                        decoration: const InputDecoration(
+                          hintText: "Search you country code...",
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: ListView.separated(
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            onTap: () {
-                              context.read<CountryViewModel>().setCountry(
-                                    snapshot.data![index],
-                                  );
-                              Navigator.pop(context);
-                            },
-                            enableFeedback: true,
-                            leading: Image.network(
-                              "https://flagcdn.com/48x36/${snapshot.data![index].code.toLowerCase()}.png",
-                              width: 30,
-                            ),
-                            title: Text(snapshot.data![index].name),
-                            subtitle: Text(snapshot.data![index].dialCode),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return const SizedBox(height: 10);
-                        },
-                        itemCount: snapshot.data?.length ?? 0,
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: ListView.separated(
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              onTap: () {
+                                context.read<CountryViewModel>().setCountry(
+                                      snapshot.data![index],
+                                    );
+                                Navigator.pop(context);
+                              },
+                              enableFeedback: true,
+                              leading: Image.network(
+                                "https://flagcdn.com/48x36/${snapshot.data![index].code.toLowerCase()}.png",
+                                width: 30,
+                              ),
+                              title: Text(snapshot.data![index].name),
+                              subtitle: Text(snapshot.data![index].dialCode),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return const SizedBox(height: 10);
+                          },
+                          itemCount: snapshot.data?.length ?? 0,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               );
             },
           ),
