5288c475a97a8198ead48d51901fb39ef0e04128
Anin Arafath <65447144+aninarafath6@users.noreply.github.com>
2022-08-13 14:23:14 +0530

    verification banner refactor

--- arnhss-app/lib/features/authentication/otp_verification/views/otp_verify_view.dart
+++ arnhss-app/lib/features/authentication/otp_verification/views/otp_verify_view.dart
@@ -1,6 +1,7 @@
 import 'package:arnhss/common/constants/color_constants.dart';
 import 'package:arnhss/common/constants/image_constant.dart';
 import 'package:arnhss/common/theme/text_theme.dart';
+import 'package:arnhss/features/authentication/otp_verification/widgets/verification_banner.dart';
 import 'package:arnhss/features/authentication/view_model/country_view_model.dart';
 import 'package:arnhss/features/authentication/view_model/login_view_model.dart';
 import 'package:arnhss/features/authentication/view_model/verify_otp_view_model.dart';
@@ -24,37 +25,7 @@ class OtpVerificationView extends StatelessWidget {
         padding: const EdgeInsets.all(24.0),
         child: ListView(
           children: [
-            Image.asset(
-              Images.otp_image,
-            ),
-            Text(
-              "Check Your Message",
-              style: CustomTextTheme(context: context).headLine(),
-              textAlign: TextAlign.center,
-            ),
-            context.spacing(height: 2),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                      text: "We've sent 6 digit code to ",
-                      style: CustomTextTheme(context: context).paragraph()),
-                  TextSpan(
-                    text:
-                        " ${context.read<CountryViewModel>().selectedCountry.dialCode}9947018118 ",
-                    style:
-                        CustomTextTheme(context: context).paragraph().copyWith(
-                              color: CustomColors.dark,
-                              fontWeight: FontWeight.w500,
-                            ),
-                  ),
-                  TextSpan(
-                      text: "for verify your mobile number ",
-                      style: CustomTextTheme(context: context).paragraph()),
-                ],
-              ),
-            ),
+            const VerificationBanner(),
             context.spacing(height: 4),
             OtpTextField(
               focusedBorderColor: CustomColors.dark,
---
+++ arnhss-app/lib/features/authentication/otp_verification/widgets/verification_banner.dart
@@ -0,0 +1,48 @@
+import 'package:arnhss/common/constants/color_constants.dart';
+import 'package:arnhss/common/constants/image_constant.dart';
+import 'package:arnhss/common/theme/text_theme.dart';
+import 'package:arnhss/features/authentication/view_model/country_view_model.dart';
+import 'package:arnhss/utils/dimensions.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class VerificationBanner extends StatelessWidget {
+  const VerificationBanner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Image.asset(
+        Images.otp_image,
+      ),
+      Text(
+        "Check Your Message",
+        style: CustomTextTheme(context: context).headLine(),
+        textAlign: TextAlign.center,
+      ),
+      context.spacing(height: 2),
+      RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "We've sent 6 digit code to ",
+              style: CustomTextTheme(context: context).paragraph(),
+            ),
+            TextSpan(
+              text:
+                  " ${context.read<CountryViewModel>().selectedCountry.dialCode}9947018118 ",
+              style: CustomTextTheme(context: context).paragraph().copyWith(
+                    color: CustomColors.dark,
+                    fontWeight: FontWeight.w500,
+                  ),
+            ),
+            TextSpan(
+                text: "for verify your mobile number ",
+                style: CustomTextTheme(context: context).paragraph()),
+          ],
+        ),
+      ),
+    ]);
+  }
+}
