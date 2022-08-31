e05d1920ea5db593aba5679b846c568f32a944e0
Anin Arafath <aninarafath@Anins-MacBook-Air.local>
2022-09-01 02:28:45 +0530

    notice dailog upgraded

--- arnhss-app/lib/features/home/widgets/notice_dailog.dart
+++ arnhss-app/lib/features/home/widgets/notice_dailog.dart
@@ -13,56 +13,53 @@ class NoticeDailog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(25),
-      padding: const EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: [
-          const SizedBox(height: 20),
-          Text(
-            "Notice!!",
-            style: CustomTextTheme(context: context).headLine(),
-          ),
-          const SizedBox(height: 20),
-
-          Expanded(
-            child: ListView(
-              children: [
-                const SizedBox(height: 30),
-                // Image.asset(
-                //   "assets/images/icons/nt-going-through-files.png",
-                //   width: 29,
-                // ),
-                // const SizedBox(height: 10),
-                Text(
-                  notice.title ?? "",
-                  textAlign: TextAlign.start,
-                  style: CustomTextTheme(context: context)
-                      .headLine()
-                      .copyWith(fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(height: 20),
-                Text(notice.descritption ?? "",
-                    textAlign: TextAlign.start,
-                    style: CustomTextTheme(context: context).paragraph()),
-                const SizedBox(height: 15),
-                Text(notice.subDescription ?? "",
-                    textAlign: TextAlign.start,
-                    style: CustomTextTheme(context: context).paragraph()),
-                const SizedBox(height: 20),
-              ],
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              "Notice!!",
+              style: CustomTextTheme(context: context).headLine(),
             ),
-          ),
-          // const Spacer(),
-          CustomButton(
-            label: "Okay",
-            onTap: () => Navigator.pop(context),
-          )
-        ],
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: ListView(
+                children: [
+                  const SizedBox(height: 30),
+                  Text(
+                    notice.title ?? "",
+                    textAlign: TextAlign.start,
+                    style: CustomTextTheme(context: context)
+                        .headLine()
+                        .copyWith(fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(notice.descritption ?? "",
+                      textAlign: TextAlign.start,
+                      style: CustomTextTheme(context: context).paragraph()),
+                  const SizedBox(height: 15),
+                  Text(notice.subDescription ?? "",
+                      textAlign: TextAlign.start,
+                      style: CustomTextTheme(context: context).paragraph()),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+            // const Spacer(),
+            CustomButton(
+              label: "Okay",
+              onTap: () => Navigator.pop(context),
+            )
+          ],
+        ),
       ),
     );
   }
