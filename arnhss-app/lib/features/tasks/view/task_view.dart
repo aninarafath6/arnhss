1117cedc8b9343c188a6f06ce335e30c93f10ec7
aninarafath6 <arafathanin6@gmail.com>
2022-09-14 00:01:33 +0530

    remoevd unecessery code

--- arnhss-app/lib/features/tasks/view/task_view.dart
+++ arnhss-app/lib/features/tasks/view/task_view.dart
@@ -1,9 +1,7 @@
 import 'package:arnhss/common/constants/color_constants.dart';
-import 'package:arnhss/common/constants/image_constant.dart';
-import 'package:arnhss/common/theme/text_theme.dart';
 import 'package:arnhss/features/tasks/view_model/task_view_model.dart';
 import 'package:arnhss/features/tasks/widgets/date_timline.dart';
-import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:arnhss/features/tasks/widgets/not_found.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -72,44 +70,6 @@ class TaskView extends StatelessWidget {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class NotFound extends StatelessWidget {
-  const NotFound({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Spacer(flex: 2),
-          Container(
-            // height: 350,
-            width: 250,
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Image.asset(Images.notFound),
-            ),
-          ),
-          const SizedBox(height: 25),
-          Text(
-            "No plans for this day",
-            style: CustomTextTheme(context: context).headLine2(),
-          ),
-          const SizedBox(height: 10),
-          // Text(
-          //   "looks like you haven't made your plans yet",
-          //   style: CustomTextTheme(context: context).paragraph(),
-          //   textAlign: TextAlign.center,
-          // ),
-          const Spacer(flex: 3),
-        ],
       ),
     );
   }
