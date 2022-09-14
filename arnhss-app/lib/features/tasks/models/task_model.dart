2117ada85a9de8869994e30e67941eb445b58fdd
aninarafath6 <arafathanin6@gmail.com>
2022-09-15 00:45:03 +0530

    task model 🧶

---
+++ arnhss-app/lib/features/tasks/models/task_model.dart
@@ -0,0 +1,23 @@
+class TaskModel {
+  final String? title;
+  final String? subTitle;
+  final DateTime? startTime;
+  final DateTime? endTime;
+  final DateTime? date;
+  final String? note;
+  final String? category;
+  final String? type;
+  final bool? isCompleted;
+
+  TaskModel({
+    this.title,
+    this.subTitle,
+    this.startTime,
+    this.endTime,
+    this.date,
+    this.note,
+    this.category,
+    this.type,
+    this.isCompleted,
+  });
+}
