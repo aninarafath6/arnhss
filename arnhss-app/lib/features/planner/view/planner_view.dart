412af1f9097f6adb03d7f1b688c5cc22cbc919af
aninarafath6 <arafathanin6@gmail.com>
2022-09-30 22:42:41 +0530

    code cleaning

--- arnhss-app/lib/features/planner/view/planner_view.dart
+++ arnhss-app/lib/features/planner/view/planner_view.dart
@@ -10,8 +10,6 @@ import 'package:arnhss/features/planner/widgets/planner_tile/planner_tile.dart';
 class PlannerView extends StatelessWidget {
   const PlannerView({Key? key}) : super(key: key);
   static const routeName = '/task_view';
-  // static final PageController _controller =
-  //     PageController(initialPage: DateTime.now().day);
 
   @override
   Widget build(BuildContext context) {
@@ -59,59 +57,6 @@ class PlannerView extends StatelessWidget {
                           ),
                   ),
                 ),
-                // Expanded(
-                //   child: PageView.builder(
-                //     physics: const NeverScrollableScrollPhysics(),
-                //     controller: context.read<PlannerViewModel>().pageController,
-                //     itemBuilder: (context, index) {
-                //       return Padding(
-                //         padding: const EdgeInsets.only(
-                //             left: 21.0, right: 21, top: 21),
-
-                //         // child: FutureBuilder<List<PlannerModel>>(
-                //         //   builder: ((context, snapshot) {
-                //         //     if (snapshot.connectionState ==
-                //         //         ConnectionState.waiting) {
-                //         //       return SingleChildScrollView(
-                //         //         physics: const NeverScrollableScrollPhysics(),
-                //         //         child: Column(
-                //         //           children: List.generate(
-                //         //             10,
-                //         //             (index) => Shimmer.fromColors(
-                //         //               baseColor: CustomColors.bgOverlay,
-                //         //               highlightColor:
-                //         //                   CustomColors.light.withOpacity(.4),
-                //         //               child: const PlanSkelton(),
-                //         //             ),
-                //         //           ),
-                //         //         ),
-                //         //       );
-                //         //     }
-                //         //     if (snapshot.data!.isEmpty) {
-                //         //       return const NotFound();
-                //         //     }
-
-                //         //     return ListView.builder(
-                //         //       physics: const PageScrollPhysics(),
-                //         //       itemBuilder: ((context, index) => PlannerTile(
-                //         //             plan: snapshot.data![index],
-                //         //           )),
-                //         //       itemCount: snapshot.data!.length,
-                //         //     );
-                //         //   }),
-                //         //   future: context
-                //         //       .read<NewPlanViewModel>()
-                //         //       .getTasksOfTheDay(context
-                //         //           .watch<PlannerViewModel>()
-                //         //           .selectedDate),
-                //         // ),
-                //       );
-                //     },
-                //     onPageChanged: (day) {
-                //       context.read<PlannerViewModel>().swipePage(day);
-                //     },
-                //   ),
-                // )
               ],
             ),
             FloatingButton(
