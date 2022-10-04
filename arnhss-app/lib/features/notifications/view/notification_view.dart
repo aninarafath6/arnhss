81d8acc8c2f4b9be8182abeaf13d04b89607a12c
aninarafath6 <arafathanin6@gmail.com>
2022-10-04 17:32:42 +0530

    refactor

--- arnhss-app/lib/features/notifications/view/notification_view.dart
+++ arnhss-app/lib/features/notifications/view/notification_view.dart
@@ -1,8 +1,7 @@
 import 'package:arnhss/common/constants/app_sizes.dart';
-import 'package:arnhss/common/constants/color_constants.dart';
 import 'package:arnhss/features/authentication/otp_verification/view/index.dart';
-import 'package:flutter/services.dart';
-import 'package:remixicon/remixicon.dart';
+import 'package:arnhss/features/notifications/widgets/notification_app_bar.dart';
+import 'package:arnhss/features/notifications/widgets/notification_tile.dart';
 
 class NotificationView extends StatelessWidget {
   const NotificationView({Key? key}) : super(key: key);
@@ -11,52 +10,19 @@ class NotificationView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _notificationsAppBar(context),
+      appBar: notificationsAppBar(context),
       body: Padding(
         padding: const EdgeInsets.symmetric(
             horizontal: AppSizes.default_padding - 15),
         child: ListView(
-          children: <Widget>[
-            Dismissible(
-              key: const ValueKey(1),
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: ListTile(
-                  title: const Text("Sunday is holiday"),
-                  subtitle: const Text("Something went sunday is holyday."),
-                  leading: CircleAvatar(
-                    backgroundColor: CustomColors.lightBgOverlay,
-                    child: const Center(
-                      child: Icon(
-                        Remix.bell_line,
-                        size: 18,
-                        color: CustomColors.light,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            )
+          children: const [
+            NotificationTile(),
+            NotificationTile(),
+            NotificationTile(),
+            NotificationTile(),
           ],
         ),
       ),
-    );
-  }
-
-  AppBar _notificationsAppBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      systemOverlayStyle:
-          const SystemUiOverlayStyle(statusBarBrightness: Brightness.light),
-      elevation: 0,
-      title: const Text(
-        "Notifications",
-        style: TextStyle(color: CustomColors.dark),
-      ),
-      leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          splashRadius: 28,
-          icon: const Icon(Icons.arrow_back_ios, color: CustomColors.dark)),
     );
   }
 }
