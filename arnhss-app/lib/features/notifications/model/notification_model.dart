172bdcd673a53482fb7709ebff82a58aa6966dac
aninarafath6 <arafathanin6@gmail.com>
2022-10-04 17:35:15 +0530

    notification model

---
+++ arnhss-app/lib/features/notifications/model/notification_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotificationModel {
+  final IconData? icon;
+  final String? title;
+  final String? subTitle;
+  final bool? isReade;
+
+  NotificationModel({this.icon, this.title, this.subTitle, this.isReade});
+}
