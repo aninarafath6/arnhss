313a703c23ffbb0357b2111738d47ef8590f8513
aninarafath6 <arafathanin6@gmail.com>
2022-10-15 00:34:45 +0530

    cant find profile page ui

--- arnhss-app/lib/features/authentication/account/view/select_account.dart
+++ arnhss-app/lib/features/authentication/account/view/select_account.dart
@@ -1,77 +1,109 @@
 import 'package:arnhss/common/constants/app_sizes.dart';
 import 'package:arnhss/common/constants/color_constants.dart';
-import 'package:arnhss/common/constants/image_constant.dart';
 import 'package:arnhss/common/widgets/custom_banner.dart';
-import 'package:arnhss/common/widgets/not_found.dart';
-import 'package:arnhss/extensions/string_extension.dart';
-import 'package:arnhss/features/authentication/account/widgets/account_tile_skeliton.dart';
+import 'package:arnhss/features/authentication/account/widgets/account_tile_skelton.dart';
+import 'package:arnhss/features/authentication/account/widgets/account_tile.dart';
 import 'package:arnhss/features/authentication/otp_verification/view/index.dart';
 import 'package:arnhss/features/authentication/repo/login_service.dart';
 import 'package:arnhss/features/home/view/home_view.dart';
 import 'package:arnhss/models/user.model.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:get/route_manager.dart';
+import 'package:shimmer/shimmer.dart';
 
-class SelectAccount extends StatelessWidget {
+class SelectAccount extends StatefulWidget {
   const SelectAccount({Key? key}) : super(key: key);
   static const routeName = "/selectAccount";
 
   @override
+  State<SelectAccount> createState() => _SelectAccountState();
+}
+
+class _SelectAccountState extends State<SelectAccount> {
+  bool isError = false;
+  @override
   Widget build(BuildContext context) {
+    LoginService().getListUsers().then(((value) {
+      if (value!.isEmpty) {
+        setState(() {
+          isError = true;
+        });
+      }
+    }));
+
     return Scaffold(
       appBar: customAppBar(context, title: "Account"),
       body: Padding(
         padding: const EdgeInsets.all(AppSizes.default_padding),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment:
+              isError ? MainAxisAlignment.center : MainAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
+            isError ? const Spacer() : SizedBox(),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: CustomBanner(
-                  image:
-                      "assets/images/icons/team-features-illustration.png.webp",
-                  title: "Find Your Profile",
-                  subtitle: "Choose an account to be proceed",
-                  isSmall: true),
-            ),
-            const SizedBox(height: 30),
-            Expanded(
-              child: FutureBuilder<List<UserModel>?>(
-                future: LoginService().getListUsers(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CupertinoActivityIndicator(
-                      color: CustomColors.dark,
-                    );
-                  }
-                  if (snapshot.data == null || snapshot.data!.isEmpty) {
-                    // Get.to(const NotExist());
-                    return const SizedBox();
-                  } else if (snapshot.data!.isNotEmpty) {
-                    return ListView.builder(
-                      itemBuilder: (context, index) {
-                        return AccountTile(
-                          user: snapshot.data?[index],
-                        );
-                      },
-                      itemCount: snapshot.data?.length,
-                    );
-                  } else {
-                    return const CupertinoActivityIndicator(
-                      color: CustomColors.dark,
-                    );
-                  }
-                },
+                image: isError
+                    ? "assets/images/icons/not_exist.png"
+                    : "assets/images/icons/team-features-illustration.png.webp",
+                title: isError ? "Can't Find Profile" : "Get Your Profile",
+                subtitle: isError
+                    ? "Your profile has not been found. Please contact your class teacher for assistance"
+                    : "Choose an account to be proceed",
+                isSmall: true,
               ),
             ),
-
-            CustomButton(
-              label: "Continue",
-              onTap: () =>
-                  Navigator.pushReplacementNamed(context, HomeView.routeName),
-            ),
             const SizedBox(height: 30),
-//
+            isError
+                ? const SizedBox()
+                : Expanded(
+                    child: FutureBuilder<List<UserModel>?>(
+                      future: LoginService().getListUsers(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: ((context, index) =>
+                                Shimmer.fromColors(
+                                  baseColor: CustomColors.bgOverlay,
+                                  highlightColor:
+                                      CustomColors.light.withOpacity(.4),
+                                  child: const AccountTileSkelton(),
+                                )),
+                            itemCount: 3,
+                          );
+                        }
+                        if (snapshot.data == null || snapshot.data!.isEmpty) {
+                          return const Center(
+                            child: Text("error"),
+                          );
+                        } else if (snapshot.data!.isNotEmpty) {
+                          return ListView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return AccountTile(
+                                user: snapshot.data?[index],
+                              );
+                            },
+                            itemCount: snapshot.data?.length,
+                          );
+                        } else {
+                          return const SizedBox();
+                        }
+                      },
+                    ),
+                  ),
+
+            isError
+                ? const SizedBox()
+                : CustomButton(
+                    label: "Continue",
+                    onTap: () => Navigator.pushReplacementNamed(
+                        context, HomeView.routeName),
+                  ),
+            const SizedBox(height: 30),
+            isError ? const Spacer() : SizedBox(),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const [
---
+++ arnhss-app/lib/features/authentication/account/widgets/account_tile_skelton.dart
@@ -0,0 +1,85 @@
+import 'package:arnhss/common/constants/color_constants.dart';
+import 'package:arnhss/common/constants/image_constant.dart';
+import 'package:arnhss/extensions/string_extension.dart';
+import 'package:arnhss/models/user.model.dart';
+import 'package:flutter/material.dart';
+
+import '../view/select_account.dart';
+
+class AccountTileSkelton extends StatelessWidget {
+  const AccountTileSkelton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CustomColors.lightBgOverlay,
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
+          ),
+          const SizedBox(width: 12),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 80,
+                height: 15,
+                color: Colors.green,
+              ),
+              const SizedBox(height: 5),
+              Container(
+                width: 120,
+                height: 10,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          const Spacer(),
+          Container(
+            width: 60,
+            height: 22,
+            decoration: const BoxDecoration(
+              color: Colors.green,
+              // borderRadius: BorderRadius.circular(25),
+            ),
+          )
+        ],
+      ),
+      height: 80,
+      margin: const EdgeInsets.only(bottom: 15),
+    );
+  }
+
+  Color getOverlayColor(Role? role) {
+    if (role == Role.student) {
+      return CustomColors.presentColor.withOpacity(.1);
+    } else if (role == Role.parent) {
+      return CustomColors.absentColor.withOpacity(.1);
+    } else {
+      return CustomColors.halfColor.withOpacity(.1);
+    }
+  }
+
+  String getRoleString(Role? role) {
+    if (role == Role.student) {
+      return "Student";
+    } else if (role == Role.parent) {
+      return "Parent";
+    } else {
+      return "Teacher";
+    }
+  }
+}
