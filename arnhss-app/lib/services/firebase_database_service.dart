7684bae12bcb1f2d778e7c8e3f1ae440cf03323a
aninarafath6 <arafathanin6@gmail.com>
2022-10-21 20:32:59 +0530

    new

--- arnhss-app/lib/services/firebase_database_service.dart
+++ arnhss-app/lib/services/firebase_database_service.dart
@@ -1,12 +1,45 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class FireBaseDatabaseService {
+import 'package:arnhss/features/home/model/notice_model.dart';
+import 'package:arnhss/services/base/exception/handle_exception.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class FireBaseDatabaseService with HandleException {
   //* collection reference
   final CollectionReference noticesCollection =
       FirebaseFirestore.instance.collection('notices');
+  // DatabaseReference ref = FirebaseDatabase.instance.ref("notices/");
 
 //* get stream
   Stream<QuerySnapshot?> get notices {
     return noticesCollection.snapshots();
+  }
+
+  Stream<NoticeModel>? get notice {
+    try {
+      DatabaseReference ref = FirebaseDatabase.instance.ref('notice');
+      return ref.onValue.map(
+        (event) => NoticeModel.fromRawJson(
+          jsonEncode(event.snapshot.value),
+        ),
+      );
+    } catch (e) {
+      handleException(e);
+    }
+    return null;
+  }
+
+  void setNotice(NoticeModel newNotice) async {
+    print("here");
+    DatabaseReference ref = FirebaseDatabase.instance.ref("notice");
+    try {
+      // Only update the name, leave the age and address!
+      await ref.update(newNotice.toJson());
+    } catch (e) {
+      print(e);
+      handleException(e);
+    }
   }
 }
