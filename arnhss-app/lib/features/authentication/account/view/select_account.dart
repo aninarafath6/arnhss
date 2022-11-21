0d403a4e6bebc54b2cf6443df32c91c194cafe4a
aninarafath6 <arafathanin6@gmail.com>
2022-11-22 00:34:07 +0530

    large file

--- arnhss-app/lib/features/authentication/account/view/select_account.dart
+++ arnhss-app/lib/features/authentication/account/view/select_account.dart
@@ -141,7 +141,7 @@ class _SelectAccountState extends State<SelectAccount> {
                                 );
                               }
                             : () {
-                                print("users array is now empty");
+                                debugPrint("users array is now empty");
                               });
                   }),
             const SizedBox(height: 30),
